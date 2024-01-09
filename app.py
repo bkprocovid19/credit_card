fed267fc312c0bf6ce25c1ead330c08f5f872ba2
bkprocovid19 <thienhaichu11@gmail.com>
2024-01-10 02:14:44 +0700

    Update interface

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sklearn.neighbors import KNeighborsClassifier
+from catboost import CatBoostClassifier
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import datetime
@@ -18,31 +19,19 @@ class PredictionHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     features = db.Column(db.String(255))
-    result = db.Column(db.String(20))
+    result = db.Column(db.String(50))
 
 
-csv_file_path = 'data\S-FFSD.csv'
+csv_file_path = 'data\S-FFSDU.xlsx'
 
 # Đọc file CSV và lưu vào một DataFrame
-data = pd.read_csv(csv_file_path)
+data = pd.read_excel(csv_file_path)
+# Lọc dữ liệu chỉ chọn những dòng có nhãn là 0 hoặc 1
 filtered_data = data[data['Labels'].isin([0, 1])]
 
-# Convert data to NumPy array
-data_np = np.array(filtered_data)
-
-# Huấn luyện encoder cho biến hạng mục 'source_account'
-source_account_encoder = OneHotEncoder(handle_unknown='ignore')
-source_account_encoded = source_account_encoder.fit_transform(data_np[:, [1,2,4,5]]).toarray()
-
-# Lấy features dạng số (numerical) từ các cột không phải biến hạng mục
-numerical_features = data_np[:, [0, 3]].astype(np.float32)
-
-# Nối features dạng số với features đã được mã hóa
-final_features = np.concatenate([numerical_features, source_account_encoded], axis=1)
-
-# Target variable
-y = data_np[:, -1].astype(np.int32)
-
+# Chia dữ liệu thành features (X) và labels (y)
+y = filtered_data['Labels'].astype(np.int32)
+X = filtered_data.drop('Labels', axis=1)
 
 def model_file_exists(filename):
     return os.path.isfile(filename)
@@ -50,17 +39,16 @@ def model_file_exists(filename):
 
 # Function to train and save the model
 def train_and_save_model():
-    # Huấn luyện mô hình KNN
-    knn_model = KNeighborsClassifier(n_neighbors=3)
-    knn_model.fit(final_features, y)
 
+# Tạo và huấn luyện mô hình CatBoost
+    model = CatBoostClassifier(iterations=500, depth=10, learning_rate=0.05, loss_function='Logloss')
+    model.fit(X, y, cat_features=['Source', 'Target', 'Location', 'Type'])
     # Lưu mô hình vào một file
-    model_filename = 'knn_model.joblib'
-    joblib.dump(knn_model, model_filename)
+    model_filename = 'cat_model.joblib'
+    joblib.dump(model, model_filename)
+    return model, model_filename
 
-    return knn_model, model_filename
-
-model_filename = 'knn_model.joblib'
+model_filename = 'cat_model.joblib'
 
 
 
@@ -87,28 +75,22 @@ def detect_fraud_knn():
     transaction_type = request.form.get('transaction_type')
 
     feature = np.array([[time,source_account,destination_account,transaction_amount,location,transaction_type]])
-
-    # Huấn luyện encoder cho biến hạng mục 'source_account'
-    source_account_encoded1 = source_account_encoder.transform([[source_account,destination_account,location,transaction_type]]).toarray()
-
-    # Preprocess the input features
-    features = np.array([[time, transaction_amount]])
-
-    # Nối features dạng số với features đã được mã hóa
-    final_features1 = np.concatenate([features, source_account_encoded1], axis=1)
+    feature1 = pd.DataFrame(feature, columns=X.columns)
 
     # Predict using the trained KNN model
-    prediction = trained_model.predict(final_features1)
-
+    prediction = trained_model.predict(feature1)
+    probabilities = trained_model.predict_proba(feature1)
+    fraud_probability_str = f'Xác suất gian lận: {round(probabilities[0][1],3)}'
     # Save prediction history
-    save_prediction_history(feature, prediction)
+    save_prediction_history(feature, fraud_probability_str)
 
-    return jsonify({'result': 'Gian lận' if prediction == 1 else 'Hợp pháp'})
+    result = {'result': 'Gian lận' if prediction[0] == 1 else 'Hợp pháp', 'fraud_probability': fraud_probability_str}
+    return jsonify(result)
 
 def save_prediction_history(feature, result):
     with app.app_context():  # Bao quanh hoạt động liên quan đến cơ sở dữ liệu
         features_str = ", ".join(map(str, feature.flatten()))
-        new_prediction = PredictionHistory(features=features_str, result='Gian lận' if result == 1 else 'Hợp pháp')
+        new_prediction = PredictionHistory(features=features_str, result = result)
         db.session.add(new_prediction)
         db.session.commit()
 
